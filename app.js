2f71301301a91335cda63ea3c97b8c307484cb5f
whtjrgus21 <whtjrgus21@naver.com>
2021-09-06 18:01:24 +0900

    프런트 단 javascript 파일 만들기

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const app = express();
 
 
 //라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 // 앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 
 app.use("/", home); //use -> 미들웨어를 등록해주는 메서드
