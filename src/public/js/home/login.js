02bdb568f434fdb825e344538f0609632d997ee8
whtjrgus21 <whtjrgus21@naver.com>
2021-09-06 22:05:44 +0900

    백엔드에서 로그인 기능 구현

--- src/public/js/home/login.js
+++ src/public/js/home/login.js
@@ -11,12 +11,14 @@ function login() {
        id: id.value,
        psword: psword.value,
     };
+    
     fetch("/login", {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         },
         body: JSON.stringify(req),
-    });
-    
+    })
+    .then((res) => res.json())
+    .then((res) => console.log(res));  
 }
